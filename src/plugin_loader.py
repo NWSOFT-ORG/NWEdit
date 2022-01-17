63696ba1e91b937eebaeae8bb6ebbc547a45d24f
ZCG-coder <z-c-ge@outlook.com>
2022-01-17 16:39:06 +0800

    Added a Node.js submodule

--- src/plugin_loader.py
+++ src/plugin_loader.py
@@ -1,3 +1,21 @@
+import importlib.util
+
+
 class Plugin:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        self.menu = None
+
+    def on_start(self):
         pass
+
+    def register_menu(self, menu):
+        self.menu = {'title': self.name,
+                     'menu': menu}
+
+
+class Loader:
+    def __init__(self):
+        spec = importlib.util.spec_from_file_location("module.name", "/path/to/file.py")
+        plug = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(plug)
